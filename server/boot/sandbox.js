72f53dfae6351d484bf5853fedfef0c7f02bb6a4
David Cheung <davidcheung@ca.ibm.com>
2016-07-18 15:18:13 -0400

    adding query part to sandbox

--- server/boot/sandbox.js
+++ server/boot/sandbox.js
@@ -18,9 +18,11 @@ module.exports = function(app) {
 
   // works fine the first time (insert)
   // update no longer populates the `folder` obj in queriedPhotoObj[0].__data
-  account.create(someAccountInfo, function(err, newAccount) {
-
+  account.findOrCreate(someAccountInfo, function(err, newAccount) {
+    console.log(err);
+    console.log(newAccount);
     newAccount.folders.create({}, function(err, folderObj){
+      console.log(folderObj);
       folderObj.photos.create({url: 'some-url'}, function(err, photoObj) {
         console.log("photoId is %s", photoObj.id);
         console.log(photoObj.folder());
@@ -38,4 +40,25 @@ module.exports = function(app) {
     });
   });
 
+  // account.findById(1, function(err, foundAccount) {
+  //   console.log(err);
+  //   console.log(foundAccount);
+  //   foundAccount.folders.create({}, function(err, folderObj){
+  //     console.log("folder: ", folderObj);
+  //     folderObj.photos.create({url: 'some-url'}, function(err, photoObj) {
+  //       console.log("photoId is %s", photoObj.id);
+  //       console.log(photoObj.folder());
+  //       photo.find(
+  //         {
+  //           where: {id: photoObj.id},
+  //           include: 'folder',
+  //         },
+  //         function(err, queriedPhotoObj) {
+  //           queriedPhotoObj[0].folderId = queriedPhotoObj[0].__data.folderId;
+  //           console.log('queried: ', queriedPhotoObj[0].folder());
+  //         }
+  //       );
+  //     });
+  //   });
+  // });
 };
